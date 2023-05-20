aacd65fe50d25b7badb5a794d9f33126faa742ce
AdelTljrd <adeltaljaard@gmail.com>
2023-05-20 14:27:13 +1200

    Changed navigation bar and checked accessibility

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 /* Sidebar navigation script */
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
+function myFunction() {
+  var x = document.getElementById("demo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
   }
-  
-  function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-  }
+}
 /* Anchor links to menu itmes script */  
   (function(document, history, location) {
     var HISTORY_SUPPORT = !!(history && history.pushState);
